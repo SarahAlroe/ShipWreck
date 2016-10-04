e5b31b6e93668f9c0241d8b982c5bfe668596547
Silas Alroe <silasalroe@gmail.com>
2016-10-04 15:15:19 +0200

    Added new classes for later use.

---
+++ src/GameLogic.java
@@ -0,0 +1,11 @@
+/**
+ * Created by silasa on 10/4/16.
+ */
+public class GameLogic {
+    private Board board1 = new Board();
+    private Board board2 = new Board();
+    private int gameState;
+    public void setup(){
+
+    }
+}
