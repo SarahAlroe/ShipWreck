b5ed8b3fba182c3cfc476b9df2d601c3724df2e0
Silas Alroe <silasalroe@gmail.com>
2016-10-11 12:39:21 +0200

    Added getPossiblePositions and Position class.

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,11 +1,41 @@
+import java.util.ArrayList;
+
 /**
  * Created by silasa on 10/4/16.
  */
-public class GameLogic {
-    private Board board1 = new Board();
-    private Board board2 = new Board();
-    private int gameState;
-    public void setup(){
 
+public class GameLogic {
+    private int boardSize = 10;
+
+    private Board board1 = new Board(boardSize);
+    private Board board2 = new Board(boardSize);
+    private int gameState;
+
+    public void setup() {
+        // load player and board, set gamestate
     }
+
+    public ArrayList<Position> getPossibleEndPositions(Board board, int x, int y, int length) {
+        ArrayList<Position> possiblePositions = new ArrayList<>();
+        int maxLengthX = board.getBoardSizeX();
+        int maxLengthY = board.getBoardSizeY();
+
+        if (x + length < maxLengthX && board.getSegment(x + length, y) == Board.NOTHING) {
+            possiblePositions.add(new Position(x + length, y));
+        }
+        if (x - length > 0 && board.getSegment(x - length, y) == Board.NOTHING) {
+            possiblePositions.add(new Position(x - length, y));
+        }
+        if (y + length < maxLengthY && board.getSegment(x, y + length) == Board.NOTHING) {
+            possiblePositions.add(new Position(x + length, y));
+        }
+        if (y - length > 0 && board.getSegment(x, y - length) == Board.NOTHING) {
+            possiblePositions.add(new Position(x, y - length));
+        }
+        return possiblePositions;
+    }
+    public ArrayList<Position> getPossibleEndPositions(Board board, Position pos, int length) {
+        return getPossibleEndPositions(board,pos.getX(),pos.getY(),length);
+    }
+
 }
---
+++ src/Position.java
@@ -0,0 +1,28 @@
+/**
+ * Created by silasa on 10/11/16.
+ */
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int nX, int nY) {
+        x = nX;
+        y = nY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
